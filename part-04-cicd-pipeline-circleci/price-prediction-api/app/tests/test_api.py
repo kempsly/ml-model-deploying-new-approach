9511c974b69f593c3e0d4f1c3da2ca8fef630c1b
kempsly <kempslysilencieux3@gmail.com>
2024-03-16 01:28:32 +0300

    Change registered in the app

--- part-04-cicd-pipeline-circleci/price-prediction-api/app/tests/test_api.py
+++ part-04-cicd-pipeline-circleci/price-prediction-api/app/tests/test_api.py
@@ -24,3 +24,4 @@ def test_make_prediction(client: TestClient, test_data: pd.DataFrame) -> None:
     assert prediction_data["predictions"]
     assert prediction_data["errors"] is None
     assert math.isclose(prediction_data["predictions"][0], 113422, rel_tol=100)
+    
