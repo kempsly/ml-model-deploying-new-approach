bb45fd405a5be2900deda484f88dfa3030e600bb
kempsly <kempslysilencieux3@gmail.com>
2024-03-16 00:02:46 +0300

    Update package

--- part-02-model-packaging-to-production/regression_model/train_pipeline.py
+++ part-02-model-packaging-to-production/regression_model/train_pipeline.py
@@ -4,7 +4,6 @@ from pipeline import price_pipe
 from processing.data_manager import load_dataset, save_pipeline
 from sklearn.model_selection import train_test_split 
 
-
 def run_training() -> None:
     """Train the model."""
 
