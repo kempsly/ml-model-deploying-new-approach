7b395be1b28f6d4cd0ec8248fc65d3cfce7c6482
kempsly <kempslysilencieux3@gmail.com>
2024-03-15 14:08:24 +0300

    Update repo

--- part-04-cicd-pipeline-circleci/model-package/setup.py
+++ part-04-cicd-pipeline-circleci/model-package/setup.py
@@ -1,35 +1,37 @@
-from pathlib import Path  
-from setuptools import find_packages, setup  
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from pathlib import Path
 
-# Package meta-data
-NAME = 'hpp-regression-model'
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'tid-regression-model'
 DESCRIPTION = 'New Approach to package model using regression algorithm.'
 URL = "https://github.com/kempsly/ml-model-deploying-new-approach"
 EMAIL = "kempslysilencieux3@gmail.com"
 AUTHOR = "Kempsly"
-REQUIRES_PYTHON = ">=3.8.18"
+REQUIRES_PYTHON = ">=3.7.0"
 
-long_description = DESCRIPTION 
+
+long_description = DESCRIPTION
 
 # Load the package's VERSION file as a dictionary.
 about = {}
 ROOT_DIR = Path(__file__).resolve().parent
 REQUIREMENTS_DIR = ROOT_DIR / 'requirements'
 PACKAGE_DIR = ROOT_DIR / 'regression_model'
-
 with open(PACKAGE_DIR / "VERSION") as f:
     _version = f.read().strip()
     about["__version__"] = _version
-    
 
-# required packages for this module to be executed?
-def list_reqs(fname='requirements.txt'):
+
+# What packages are required for this module to be executed?
+def list_reqs(fname="requirements.txt"):
     with open(REQUIREMENTS_DIR / fname) as fd:
         return fd.read().splitlines()
-    
 
-
+# Where the magic happens:
 setup(
     name=NAME,
     version=about["__version__"],
@@ -41,13 +43,14 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=("tests",)),
-    package_data ={"regression_model": ["VERSION"]},
+    package_data={"regression_model": ["VERSION"]},
     install_requires=list_reqs(),
     extras_require={},
     include_package_data=True,
-    licence="BSD-3",
+    license="BSD-3",
     classifiers=[
         # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
@@ -55,13 +58,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
-],
-
-    
+    ],
 )
-
-
-
